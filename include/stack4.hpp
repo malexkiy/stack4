c3468cb4e0924c180e85c32bbdd85676d4a6856b
malexkiy <malexkiy@gmail.com>
2017-10-27 12:59:36 +0300

    Create stack4.hpp

---
+++ include/stack4.hpp
@@ -0,0 +1,95 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <algorithm>
+#include <utility>
+
+template <typename T>
+class stack
+{
+public:
+	stack() noexcept;
+	size_t count() const noexcept;
+	void push(T const &) /* strong */;
+	void pop() /* strong */;
+	T top() const /* strong */;
+
+private:
+	void swap(stack<T>&) noexcept;
+	T * array_;
+	size_t array_size_;
+	size_t count_;
+};
+
+
+template <typename T>
+stack<T>::stack()
+	: count_(0), array_size_(0), array_(nullptr) noexcept
+{
+}
+
+
+template <typename T>
+void stack<T>::swap(stack<T>& obj) noexcept
+{
+	using std::swap;
+
+	swap(count_, obj.count_);
+	swap(array_size_, obj.array_size_);
+	swap(array_, obj.array_);
+}
+
+
+template <typename T>
+size_t stack<T>::count() const noexcept
+{
+	return count_;
+}
+
+
+template <typename T>
+void stack<T>::push(T const& obj) /* strong */
+{
+	if (count_ == array_size_)
+	{
+		auto array_size = array_size_ == 0 ? 1 : array_size_ * 2;
+		T* narray = new T[array_size];
+		std::copy(array_, array_ + count_, narray);
+
+		array_size_ = array_size;
+
+		if (array_)
+			delete[] array_;
+		array_ = narray;
+	}
+
+	array_[count_] = obj;
+	++count_;
+}
+
+
+template <typename T>
+T stack<T>::top() const /* strong */
+{
+	if (count_ == 0)
+	{
+		throw "Stack is empty!";
+	}
+
+	return array_[count_ - 1];
+}
+
+
+template <typename T>
+void stack<T>::pop() /* strong */
+{
+	if (count_ == 0)
+	{
+		throw "Stack is empty!";
+	}
+
+	count_--;
+}
+
+
+#endif
